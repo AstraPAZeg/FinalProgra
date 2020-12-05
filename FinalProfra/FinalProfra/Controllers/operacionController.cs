478b67c6fec8301c0e44313b1f2c73ac81a4c5ab
Paul Aguilar Zegada <a2019113015@estudiantes.upsa.edu.bo>
2020-12-05 12:16:01 -0400

    WS completo, con la funcionalidad de dado un número muestra uno u otro mensaje según las condiciones en el enunciado

---
+++ FinalProfra/FinalProfra/Controllers/operacionController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProfra.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class operacionController : ControllerBase
+    {
+        [HttpGet]
+        public string funcion (int numero) {
+            string texto="";
+            if (numero > 0) {
+                texto= "Usted ingresó el número  "+ numero;
+            }
+            if (numero < 0)
+            {
+                texto = "ERROOOR";
+            }
+            if (numero == 0)
+            {
+                texto = "Realizado por Paul Aguilar";
+            }
+            return texto;
+        }
+    }
+}
